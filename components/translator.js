dceada520d7f5d46763d4740bdcc70382e1a4e37
Mikulas Machovec <mikimachovec@gmail.com>
2025-02-22 19:03:17 +0000

    americanToBriish part done

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,37 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+
+    validate(inputString){
+
+    }
+
+    aremicanToBritish(inputString){
+        const inputArray = inputString.split(" ");
+        let translation = []
+        for(let i = 0; i < inputArray.length; i++ ){
+            let word =inputArray[i].toLowerCase()
+
+            if(americanOnly[word]){
+                translation.push(americanOnly[word])
+            } else if(americanToBritishSpelling[word]){
+                translation.push(americanToBritishSpelling[word])
+            } else if(americanToBritishTitles[word]){
+                let translatedTitle = americanToBritishTitles[word]
+                if(inputArray[i][0]=== inputArray[i][0].toUpperCase()){
+                    translatedTitle = translatedTitle.charAt(0).toUpperCase() + translatedTitle.slice(1);
+                }
+                translation.push(translatedTitle)
+            } else {
+                translation.push(inputArray[i])
+            }              
+        }
+        //return
+        console.log(translation)
+    }
+
+    britishToAmerican(){}
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const { text , locale} = req.body;
+      console.log(text.toLowerCase())
+      console.log(text.toUpperCase())
+      if(!text){
+        return res.json({ error : 'No text to translate'})
+      }
+
+      if(!text || !locale){
+        return res.json({error: 'Required field(s) missing'})
+      }
+
+      if(locale === 'american-to-british'){
+        translator.aremicanToBritish(text);
+      } else if(locale === 'british-to-american'){
+        translator.britishToAmerican(text);
+      }else{
+        return res.json({error: 'Invalid value for locale field'})
+      }
       
     });
 };
